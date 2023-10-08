8ac8ff21ee5b4994e32516c93fba980621d691d8
MD SHAMSER ALAM <75249067+shams-cyber@users.noreply.github.com>
2023-10-08 08:11:34 +0000

    02dataType

---
+++ 01basic/02dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+let age = 18
+let name = "sham"
+let isReady = true
+let state;
+
+//number => 2 power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone vale basically it says empty 
+//undefined => yet not define anything in variable
+//symbol=> basically it use for represent unique compnent
+
+//object
+
+console.log(typeof null);  //it gives object datatype
+console.log(typeof state);  //it gives undefined datatype
+
+
