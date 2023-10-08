ca64af5deaa96a285ef34c5a19627ecc1a0281a5
MD SHAMSER ALAM <75249067+shams-cyber@users.noreply.github.com>
2023-10-08 08:05:02 +0000

    01variable

---
+++ 01basic/01variable.js
@@ -0,0 +1,12 @@
+const accountId = 1234;
+let accountEmail = "sham@gmail.com";
+var pass = "sham";
+let accountState;
+
+//accountId = 1345; //not allowed modification in const variable
+
+console.table([accountId,accountEmail,pass,accountState]);
+
+
+
+// console.log(accountId);
