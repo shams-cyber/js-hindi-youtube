cad2daa8786f585d9737559a855e1fbb4477563b
MD SHAMSER ALAM <75249067+shams-cyber@users.noreply.github.com>
2023-10-08 09:50:01 +0000

    Conversion of datatypes

---
+++ 01basic/03conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "33abc"
+let score = null
+
+//console.log(typeof score)
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);  // make conversion string to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);         // it gives NaN 
+
+// "33" => 33
+//"33abc" => NaN type number hai
+//true => 1; false => 0
+
+let isLog = "sha";
+
+let booleanIsLog = Boolean(isLog);
+// console.log(booleanIsLog);
+
+// 1 => true | 0 => false
+// "" => false
+// "sham" => true
+
+let someNo = 33;
+let stringNumber = String(someNo);
+console.log(typeof stringNumber);
