6c9132b8f2ed8a609dbb5d9c92cf5432de7c5d7a
MD SHAMSER ALAM <75249067+shams-cyber@users.noreply.github.com>
2023-10-09 12:16:02 +0000

    basic of string

---
+++ 01basic/05string.js
@@ -0,0 +1,52 @@
+let name = "shams" // first way to create string
+
+//another way to create string 
+
+let naam = new String("alam");
+
+// console.log(`full name is ${name} ${naam}`);
+
+// there are variety of method in string to perform different task
+
+// 1 => length
+// console.log(naam.length); size of string 4
+
+// 2 => charAt() extract character from given index
+// console.log(naam.charAt(2)); // start from index 0 
+
+// 3 => indexOf() it gives the first occurence of character in string
+
+// console.log(naam.indexOf('a'));
+
+// 4 => toUpperCase() and toLoweCase() convert capital and small
+
+// console.log(naam.toUpperCase());
+
+// 5 => substring(start,end) it take out the some part of string from given index to last index
+
+//let try to extract the some character from given string store in new variable
+
+// let cut = naam.substring(0,4);
+// console.log(cut);
+
+// 6 => slice()
+
+let string = "  naam  ";
+
+// let newString = naam.slice(0,3);
+// console.log(newString);
+
+// 7 => trim() remove the left and right space of single word
+// let newString = string.trim();
+// console.log(newString);
+
+// 8 => split() => take a pattern and matching that pattern it divide the string into list of subString it return a array
+
+// let don = "don ko pakdna muskil nhi namukin hai";
+// let store = don.split(' ');
+// console.log(Array.isArray(store));
+// console.log(store);
+
+ 
+
+
