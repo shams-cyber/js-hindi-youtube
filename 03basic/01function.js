957cd046483276552260c7fdbeba456a97655cf1
MD SHAMSER ALAM <75249067+shams-cyber@users.noreply.github.com>
2023-10-15 11:37:21 +0000

    basic of function

--- 03basic/01function.js
+++ 03basic/01function.js
@@ -42,3 +42,41 @@
 // }
 
 // console.log(login());
+
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< how to take multiple argument in paraeter and store in array >>>>>>>>>>>>>>>
+
+// function calculateCartPrice(num){
+//     return num;
+// }
+
+// console.log(calculateCartPrice(12,3,4));
+
+// SO ABOVE CODE TAKE ONLY ONE VALUE AS A PARAMETER TO OVERCOME THIS WE HAVE REST PARAMETER (...REST )
+// USED IN FUNCTION AS A PARAMETER
+
+function calculateCartPrice(...num){
+    return num;
+}
+
+// console.log(calculateCartPrice(12,34,57,88));
+
+// Rest parameter take multiple argument as parameter and store in array and return a array
+
+// ONE OF THE BEST REAL WORLD EXAMPLE IS FLIPKAR ADD TO CART TO CALCULTAE TOTAL PRICE
+
+
+// <<<<<<<<<<<<<<<<<<<<< PASSING OBJECT AS ARGUMENT IN FUNCTION CALL >>>>>>>>>>>>>>>>>>>>>>>
+
+function printPriceAndCourse(shamsKiCourse){
+    return `${shamsKiCourse.course} course price is ${shamsKiCourse.price}`
+}
+
+console.log(printPriceAndCourse({
+    course : "js ka code",
+    price : 999
+}));
+
+//  <<<<<<<<<<<<<<<<<<<< SAME WAY WE CAN PASS AN ARRAY AS ARGUMENT IN FUNCTION IN JS >>>>>>>>>>>>>>>>>>>>
+
